6013f971a391f9d739cd32b89b8b696d88cab3c8
turbyoo <kkrzeslowsky@gmail.com>
2023-02-28 19:11:56 +0100

    Added models

---
+++ WeatherForecastAPI/Classes/AverageWeatherForecast.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForecastAPI.Classes
+{
+    public class AverageWeatherForecast
+    {
+        public double Temp { get; set; }
+        public double Pressure { get; set; }
+        public double WindSpeed { get; set; }   
+
+    }
+}
---
+++ WeatherForecastAPI/Classes/CityWeatherForecast.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherForecastAPI.Classes
+{
+    public class CityWeatherForecast
+    {
+        public string City { get; set; }
+        public double Temperature { get; set; } 
+        public double Pressure { get; set; }    
+        public double WindSpeed { get; set; }
+
+    }
+}
